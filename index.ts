adbdfcdb0e7cbe9dc945a3ba9770f3ef98c0e56a
Arnav Mishra <arnavdrsk7633@gmail.com>
2021-12-28 19:09:24 +0530

    v13

--- index.ts
+++ index.ts
@@ -1,18 +1,16 @@
-import fetch from "node-fetch"
+import fetch from "node-fetch";
 const get = (query, message) => {
-    const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(query)}`;
-    fetch(url)
-
-    .then(res => res.json())
-    .then(embed => {
-
-        if (embed && !embed.error) {
-
-            message.channel.send({ embed });
-        } else {
-
-            message.reply(`No Such Result Found for **"${query}"**`);
-        }
-    })
-}
-module.exports.get = get
+  const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(
+    query
+  )}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((embed) => {
+      if (embed && !embed.error) {
+        message.channel.send({ embeds: { embed } });
+      } else {
+        message.reply(`No Such Result Found for **"${query}"**`);
+      }
+    });
+};
+export { get };
